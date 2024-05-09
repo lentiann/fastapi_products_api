94b38f24b48bb1fefb9570c360f6acb58f4d7977
Lentian Latifi <lentianlatifi@gmail.com>
2024-05-09 20:19:51 +0200

    Changes for docker-compose

--- app/utils/database.py
+++ app/utils/database.py
@@ -1,7 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLACHEMY_DATABASE_URL = "mysql+mysqlconnector://root:123456@localhost/shop"
+# SQLACHEMY_DATABASE_URL = "mysql+mysqlconnector://root:123456@localhost/shop"
+SQLACHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("SQLACHEMY_DATABASE_URL", SQLACHEMY_DATABASE_URL)
 
 engine = create_engine(SQLACHEMY_DATABASE_URL)
 
